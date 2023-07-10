96e531e008ec3f356c1574cc7a5cdb7e26785a27
Luca <luca.milde@gmx.net>
2023-07-10 22:23:06 +0200

    matrixRemove() almost finished, added a degree variable to the Vertices along with get/set methods for it

--- Graph.py
+++ Graph.py
@@ -12,7 +12,7 @@ class Graph:
         if self._getIndex(name) == -1:  #if vertex doesn't exist already
             self.vertices.append(Vertex(name))
             self.current_vertices += 1
-            self.__matrix()
+            self.__matrixAdd()
             return True
         else:
             return False
@@ -23,6 +23,7 @@ class Graph:
         else:  #remove Vertex from array
             self.vertices.pop(self._getIndex(name))
             self.current_vertices -= 1
+            self.__matrixRemove(name)
             return False
 
     def addEdge(self, start: str, end: str) -> bool:  # add a new Edge to the Graph
@@ -36,6 +37,8 @@ class Graph:
             self.edges[x][y] = 1
             self.edges[y][x] = 1
             self.current_edges += 1
+            self.vertices[x].setDegree(self.vertices[x].getDegree() + 1)
+            self.vertices[y].setDegree(self.vertices[y].getDegree() + 1)
             return True
 
     def removeEdge(self, start: str, end: str) -> bool:  #remove an existing edge from the graph
@@ -49,11 +52,13 @@ class Graph:
             self.edges[x][y] = 0
             self.edges[y][x] = 0
             self.current_edges -= 1
+            self.vertices[x].setDegree(self.vertices[x].getDegree() - 1)
+            self.vertices[y].setDegree(self.vertices[y].getDegree() - 1)
             return True
 
-    def __matrix(self) -> None:  #updates the Adjacency Matrix when a new Vertex is added
+    def __matrixAdd(self) -> None:  #updates the Adjacency Matrix when a new Vertex is added
         self.edges.append([])  #creates a new row in the matrix
-        for j in range(0, len(self.edges) - 1):  #fill the new row without the last element
+        for j in range(0, len(self.edges) - 1):  #fill the new row without the last element (which is added with the column)
             self.edges[len(self.edges) - 1].append(0)
         for i in range(0, len(self.edges)):  #create a new column. same vertex intersections are numbered "-1" everything else "0"
             if i == len(self.edges) - 1:
@@ -61,14 +66,21 @@ class Graph:
             else:
                 self.edges[i].append(0)
 
-    def _getIndex(self, name) -> int:  #returns the index of a Vertex in the vertex array, returns -1 if vertex doesnt exist
+    #TODO: update edge counter
+    def __matrixRemove(self, name) -> None:  #updates the Adjacency Matrix when a Vertex is removed
+        x = self._getIndex(name)
+        for i in range(0, len(self.edges)):  #remove column
+            self.edges[i].pop(x)
+        self.edges.pop(x)  #remove row
+
+    def _getIndex(self, name) -> int:  #returns the index of a Vertex in the vertex array, returns -1 if vertex doesn't exist
         for i in range(0, len(self.edges)):
             if self.vertices[i].getContent() == name:
                 return i
         return -1
 
     def isComplete(self) -> bool:  #checks if a graph is complete
-        if (self.current_vertices*(self.current_vertices -1))/2 == self.current_edges:
+        if (self.current_vertices*(self.current_vertices -1))/2 == self.current_edges:  #if the formula for maximum edges in a Graph is equal to the current number of edges in the Graph
             return True
         else:
             return False
--- Vertex.py
+++ Vertex.py
@@ -2,6 +2,13 @@ class Vertex:
 
     def __init__(self, name : str):
         self.__content : str = name
+        self.__degree : int = 0
 
     def getContent(self) -> str:
         return self.__content
+
+    def getDegree(self) -> int:
+        return self.__degree
+
+    def setDegree(self, degree: int) -> None:
+        self.__degree = degree
