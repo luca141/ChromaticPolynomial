25eba4d5259c5a65d0bbcefd264e23904418b385
Luca <luca.milde@gmx.net>
2023-08-31 18:39:15 +0200

    added comments

--- ChromaticPolynomial.py
+++ ChromaticPolynomial.py
@@ -11,17 +11,17 @@ class ChromaticPolynomial:
         return self.polynomial
 
     def calculatePolynomial(self) -> str:
-        isolated = self.graph.countIsolatedVertices(True)
+        isolated = self.graph.countIsolatedVertices(True) #counts and removes isolated vertices from the graph
 
-        if len(self.graph.vertices) == 0 and isolated == 0:
+        if len(self.graph.vertices) == 0 and isolated == 0: #if no vertices in graph
             self.polynomial = "0"
             return self.polynomial
 
-        elif len(self.graph.vertices) == 0 and isolated > 0:
+        elif len(self.graph.vertices) == 0 and isolated > 0: #if only isolated vertices
             self.polynomial = "x**{}".format(str(isolated))
             return self.polynomial
 
-        elif self.graph.isComplete():
+        elif self.graph.isComplete(): #if graph is complete
             self.polynomial = "1"
             for i in range(0, len(self.graph.vertices)):
                 self.polynomial += "*(x-{})".format(str(i))
@@ -29,20 +29,21 @@ class ChromaticPolynomial:
             #self.polynomial = self.simplify(self.polynomial)
             return self.polynomial
 
-        else:
-            lowest_degree_vertex = self.graph.vertices[self.graph.getLowestDegreeVertex()].getContent()
-            for i in range(0, len(self.graph.edges)):
+        else: #if neither of the above use the deletion-contraction algorythm
+            lowest_degree_vertex = self.graph.vertices[self.graph.getLowestDegreeVertex()].getContent() # look for lowest degree vertex, prevents unnecessary amount calculations
+            for i in range(0, len(self.graph.edges)): #get closest vertex to the lowest degree vertex
                 if self.graph.edges[self.graph._getIndex(lowest_degree_vertex)][i] > 0:
                     nearest_vertex = self.graph.vertices[i].getContent()
                     break
 
-            graph_removed = copy.deepcopy(self.graph)
-            graph_removed.removeEdge(lowest_degree_vertex, nearest_vertex)
+            graph_removed = copy.deepcopy(self.graph) #copy graph
+            graph_removed.removeEdge(lowest_degree_vertex, nearest_vertex) #remove edge between the two vertices chosen above
             polynomial_removed = ChromaticPolynomial(graph_removed)
             del graph_removed
-            polynomial_removed_string = polynomial_removed.calculatePolynomial()
+            polynomial_removed_string = polynomial_removed.calculatePolynomial() #calculate polynomial of graph without edge
             del polynomial_removed
 
+            #same as the block above but merges the vertices instead of removing the edge
             graph_merged = copy.deepcopy(self.graph)
             graph_merged.merge(lowest_degree_vertex, nearest_vertex)
             polynomial_merged = ChromaticPolynomial(graph_merged)
